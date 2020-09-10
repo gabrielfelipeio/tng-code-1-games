bbb63e5f0f552a99af92054f6447a340c373396c
Gabriel Felipe <gabrisilvas@outloook.com>
2020-09-10 00:51:22 -0300

    [improvement]{adding script divination.py}

---
+++ divination.py
@@ -0,0 +1,16 @@
+print("|---------------------------------|")
+print("| welcome to the divination game! |")
+print("|---------------------------------|")
+
+secret_number = 42
+
+kick = int(input("-> enter your number: "))
+
+print("- you typed:", kick)
+
+if(secret_number == kick):
+    print("- you're right!")
+else:
+    print("- you missed...")
+
+print("end game.")
