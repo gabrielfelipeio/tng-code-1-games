7f12d5c7cc16660ec0a3a38dc925d3f698ca6c4e
Gabriel Felipe <gabrisilvas@outloook.com>
2020-09-13 19:43:59 -0300

    [improvement]{chage while for a for function}

--- divination.py
+++ divination.py
@@ -4,24 +4,28 @@ print("|---------------------------------|")
 
 secret_number = 42
 total_attempts = 3
-current_attempts = 1
 
-while(current_attempts <= total_attempts):
-    print("Attempt ", current_attempts, " of ", total_attempts, " total.")
-    kick = int(input("-> enter your number: "))
+# while(current_attempts <= total_attempts):
+for current_attempts in range(1, total_attempts + 1):
+    print("Attempt {} of {} total.".format(current_attempts, total_attempts))
+    kick = int(input("-> enter a number between 1 and 100: "))
     print("- you typed:", kick)
+
+    if(kick < 1 or kick > 100):
+        print("you must enter a number between 1 and 100")
+        continue
+
     accept = secret_number == kick
     biggerThan = secret_number < kick
     lessThan = secret_number > kick
 
     if(accept):
         print("- you're right!")
+        break
     else:
         if(biggerThan):
             print("- you missed... the kick was bigger than the secret number")    
         elif(lessThan):
             print("- you missed... the kick was less than the secret number")    
-    
-    current_attempts = current_attempts + 1
     
 print("end game.")
