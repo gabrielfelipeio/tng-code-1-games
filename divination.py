10ae9f58d1523c2409d037f0d26a4a3b9eb6bc53
Gabriel Felipe <gabrisilvas@outloook.com>
2020-09-13 20:24:17 -0300

    [improvement]{using range and randrange function}

--- divination.py
+++ divination.py
@@ -1,18 +1,30 @@
+import random
+
 print("|---------------------------------|")
 print("| welcome to the divination game! |")
 print("|---------------------------------|")
 
-secret_number = 42
-total_attempts = 3
+secret_number = random.randrange(1,101)
+total_attempts = 0
+points = 1000
 
-# while(current_attempts <= total_attempts):
+print("Levels of play, when (1) is easy, (2) is medium and (3) is hard.")
+level = int(input("Set the level: "))
+
+if(level == 1):
+    total_attempts = 20
+elif(level == 2):
+    total_attempts = 10
+else:
+    total_attempts = 5 
+
 for current_attempts in range(1, total_attempts + 1):
     print("Attempt {} of {} total.".format(current_attempts, total_attempts))
-    kick = int(input("-> enter a number between 1 and 100: "))
-    print("- you typed:", kick)
+    kick = int(input("Enter a number between 1 and 100: "))
+    print("You typed:", kick)
 
     if(kick < 1 or kick > 100):
-        print("you must enter a number between 1 and 100")
+        print("You must enter a number between 1 and 100")
         continue
 
     accept = secret_number == kick
@@ -20,12 +32,14 @@ for current_attempts in range(1, total_attempts + 1):
     lessThan = secret_number > kick
 
     if(accept):
-        print("- you're right!")
+        print("You're right! and did {} points!".format(points))
         break
     else:
         if(biggerThan):
-            print("- you missed... the kick was bigger than the secret number")    
+            print("You missed... the kick was bigger than the secret number")    
         elif(lessThan):
-            print("- you missed... the kick was less than the secret number")    
+            print("You missed... the kick was less than the secret number")    
+        loss_points = abs(secret_number - kick)
+        points = points - loss_points
     
-print("end game.")
+print("End game.")
