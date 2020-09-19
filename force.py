c9b05b2c3b0fc4bfd454205ed364edb86365c387
Gabriel Felipe <gabrisilvas@outloook.com>
2020-09-18 23:24:02 -0300

    [improvement]{increment gitignore do exclude __pycache__}

--- force.py
+++ force.py
@@ -4,8 +4,17 @@ def play():
     print("|---------------------------------|")
     print("|    Welcome to the FORCE game!   |")
     print("|---------------------------------|")
-        
-    print("End game.")  
+
+    secret_word = "banana"
+    hanged = False
+    get_it_right = False
+    
+    while(not hanged and not get_it_right):
+        kick = input("What's the word? \nset your kick: ")
+        print("playing...")
+
+    print("End game.")
+
 
 if(__name__ == "__main__"):
     play()
