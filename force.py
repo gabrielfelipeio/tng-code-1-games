29e124b027326f5d37ef36c11b5358e55393a9d6
Gabriel Felipe <gabrisilvas@outloook.com>
2020-09-18 23:57:00 -0300

    [improvement]{update force game adding correct letters list to show}

--- force.py
+++ force.py
@@ -6,12 +6,25 @@ def play():
     print("|---------------------------------|")
 
     secret_word = "banana"
+    correct_letters = ["_", "_", "_", "_", "_", "_"]
+
     hanged = False
     get_it_right = False
-    
+
     while(not hanged and not get_it_right):
-        kick = input("What's the word? \nset your kick: ")
-        print("playing...")
+
+        kick_letter = input("What's the word ? -> set your kick letter: ")
+        kick_letter = kick_letter.strip()
+
+        index = 0
+        for letter in secret_word:
+            if(kick_letter.upper() == letter.upper()):
+                correct_letters[index] = letter
+                # print("Did found the letter '{}' in the position '{}', into the secret word!".format(
+                #     kick_letter, index))
+            index = index + 1
+
+        print(correct_letters)
 
     print("End game.")
 
